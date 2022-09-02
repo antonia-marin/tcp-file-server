0b9d5b4861690c2a8e7a6cb0efc1bb70febaa396
Antonia Marin <maamaringr@unal.edu.co>
2022-09-02 06:59:49 -0500

    Add handle connection in the server for the commands of the protocol.

--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"log"
 	"net"
 )
@@ -8,6 +9,51 @@ import (
 type server struct {
 	channels map[string]*channel
 }
+
+func (s *server) handleConnection(conn net.Conn) {
+	for {
+		b := make([]byte, 30112)
+		_, err := conn.Read(b)
+		if err != nil {
+			log.Printf("Unable accept the request: %s", err.Error())
+			return
+		}
+
+		cmd, chann, arg, cont := bytesParse(b)
+
+		switch cmd {
+		case "subscribe":
+			s.subscribe(conn, chann, arg)
+		case "channels":
+			s.listChannels(conn)
+		case "send":
+			s.send(conn, chann, arg, cont)
+		case "quit":
+			s.quit(conn, arg)
+		}
+	}
+}
+
+func bytesParse(b []byte) (string, string, string, []byte) {
+	cmdN := bytes.Index(b[:16], []byte{0})
+	channN := bytes.Index(b[16:48], []byte{0})
+	argsN := bytes.Index(b[48:112], []byte{0})
+
+	cmd := string(b[:cmdN])
+	channel := string(b[16:48][:channN])
+	args := string(b[48:112][:argsN])
+	content := b[112:]
+
+	return cmd, channel, args, content
+}
+
+func (s *server) subscribe(c net.Conn, chanN string, arg string) {}
+
+func (s *server) listChannels(c net.Conn) {}
+
+func (s *server) send(c net.Conn, chann string, arg string, cont []byte) {}
+
+func (s *server) quit(c net.Conn, arg string) {}
 
 func main() {
 	ln, err := net.Listen("tcp", ":9999")
@@ -19,5 +65,17 @@ func main() {
 
 	s := &server{
 		channels: make(map[string]*channel),
+	}
+
+	for {
+		connection, err := ln.Accept()
+
+		if err != nil {
+			log.Printf("Unable accept connection: %s", err.Error())
+			continue
+		}
+
+		log.Printf("New client has connected: %s", connection.RemoteAddr().String())
+		go s.handleConnection(connection)
 	}
 }
