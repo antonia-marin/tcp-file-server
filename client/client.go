8983d3e44ff09685c2bf578345e020e82438aa4c
Antonia Marin <maamaringr@unal.edu.co>
2022-09-05 08:29:00 -0500

    Create logic from the client read and send a file to the server.

--- client/client.go
+++ client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"os"
@@ -79,7 +80,22 @@ func buildChannelsPayload() []byte {
 	return commBytes
 }
 
-func (c *client) buildSendPayload(arguments []string) []byte { return nil }
+func (c *client) buildSendPayload(arguments []string) []byte {
+	byteFile := readFile(arguments[1])
+	if byteFile == nil {
+		return nil
+	}
+
+	commBytes := make([]byte, 16)
+	copy(commBytes, "send")
+
+	channelBytes := make([]byte, 32)
+	copy(channelBytes, c.channel)
+	requestBytes := append(commBytes, channelBytes...)
+	requestBytes = append(requestBytes, byteFile...)
+
+	return requestBytes
+}
 
 func buildQuitPayload() []byte {
 	commBytes := make([]byte, 16)
@@ -92,6 +108,37 @@ func (c *client) request(request []byte) {
 	if request != nil && len(request) > 0 {
 		c.conn.Write(request)
 	}
+}
+
+func readFile(filePath string) []byte {
+	fileInfo, err := os.Stat(filePath)
+	if err != nil {
+		fmt.Printf("OS. Stat() function execution error, error is:% v \n", err)
+		return nil
+	}
+
+	fileNameBytes := make([]byte, 64)
+	copy(fileNameBytes, fileInfo.Name())
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Printf("OS. Open() function execution error, error is:% v \n", err)
+		return nil
+	}
+
+	defer file.Close()
+
+	fileBytes := make([]byte, 30112)
+	n, err := file.Read(fileBytes)
+	if err != nil {
+		if err != io.EOF {
+			fmt.Printf("file. Read() method execution error, error is:% v \n", err)
+		}
+		return nil
+	}
+
+	requestBytes := append(fileNameBytes, fileBytes[:n]...)
+	return requestBytes
 }
 
 func main() {
